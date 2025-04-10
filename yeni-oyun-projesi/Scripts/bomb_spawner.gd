2b14472cfd03a82be1d9e31f198a9ca5271b2d42
Musa Eren YAKUT <m.eren.yakut@outlook.com>
2025-04-10 23:01:44 +0300

    Changed bomb and bombspawner

--- yeni-oyun-projesi/Scripts/bomb_spawner.gd
+++ yeni-oyun-projesi/Scripts/bomb_spawner.gd
@@ -2,8 +2,34 @@ extends Node2D
 
 var bomb = preload("res://Scenes/Bomb.tscn")
 
-func _on_timer_timeout() -> void:
+var spawnPosTop = [Vector2(300, 0), Vector2(450, 0), Vector2(600, 0)]
+var spawnPosLeft = [Vector2(0, 300), Vector2(0, 450), Vector2(0, 600)]
+var spawnPosRight = [Vector2(900, 300), Vector2(900, 450), Vector2(900, 600)]
+var spawnPosBot = [Vector2(300, 900), Vector2(450, 900), Vector2(600, 900)]
+var spawnPosSelection = ["spawnPosTop", "spawnPosBot", "spawnPosLeft", "spawnPosRight"]
+var spawnSelector = "spawnPosRight"
+var horizontalVel = Vector2(500, 0)
+func _on_timer_for_spawn_timeout() -> void:
 	var bombInstance = bomb.instantiate()
 	add_child(bombInstance, true)
-	bombInstance.position = Vector2(100,100)
-	print_debug(bombInstance.name)
+	if spawnSelector == "spawnPosTop":
+		bombInstance.position = spawnPosTop[randi_range(0, 2)]
+		bombInstance.get_node("BombSprite").flip_v = true
+	if spawnSelector == "spawnPosBot":
+		bombInstance.position = spawnPosBot[randi_range(0, 2)]
+		bombInstance.get_node("BombSprite").flip_v = false
+		bombInstance.velocity = -bombInstance.velocity
+	if spawnSelector == "spawnPosLeft":
+		bombInstance.position = spawnPosLeft[randi_range(0, 2)]
+		bombInstance.get_node("BombSprite").rotation_degrees = 90
+		bombInstance.velocity = horizontalVel
+	if spawnSelector == "spawnPosRight":
+		bombInstance.position = spawnPosRight[randi_range(0, 2)]
+		bombInstance.velocity = -horizontalVel
+		bombInstance.get_node("BombSprite").rotation_degrees = -90
+	#bombInstance.position
+	#print_debug(bombInstance.name)
+
+func _on_timer_for_spawn_pos_timeout() -> void:
+	spawnSelector = spawnPosSelection[randi_range(0, 3)]
+	print(spawnSelector)
